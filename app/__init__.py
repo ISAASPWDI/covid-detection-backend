f2a23c17dd197b2f31615a2b8e6178a972644cbb
Stevens Aliaga <stivensaliaga@gmail.com>
2026-01-05 10:24:10 -0500

    inserting aiven configuration into init.py and adding some dependencies in ./requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,29 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+
+    # ruta absoluta del certificado ca.pem en la raíz
+    ca_path = os.path.join(os.getcwd(), "ca.pem")
+    
+    # argumentos de conexión SSL en SQLAlchemy
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "connect_args": {
+            "ssl": {
+                "ca": ca_path
+            }
+        }
+    }
+
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
-    # cors.init_app(app, resources={r"/api/*": {"origins": "*"}}) # development
+    
+    # producción
     cors.init_app(app, resources={
-    r"/api/*": {
-        "origins": ["https://covid-detection-app.netlify.app/", "http://localhost:3000"]
-    } #production
-})
-
+        r"/api/*": {
+            "origins": ["https://covid-detection-app.netlify.app", "http://localhost:3000"]
+        }
+    })
 
     from app import models  
 
